739082ee6c3eb765345fc3a1c0a72cbb5bfe2be9
Marcel Widmer <marzelwidmer@gmail.com>
2018-04-21 18:02:13 +0200

    Initial Commit

---
+++ ApiDataWithImages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApiDataWithImages
+//
+//  Created by Marcel Widmer on 21.04.18.
+//  Copyright © 2018 Marcel Widmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
